0707a6e3e94b9a261d48025ab5a5f64dc2494840
brueck1988 <brueck1988@gmail.com>
2021-02-25 09:38:32 -0700

    Car class age method passes

--- lib/car.rb
+++ lib/car.rb
@@ -2,13 +2,17 @@ class Car
   attr_reader :color,
               :make,
               :model,
-              :year
+              :year,
+              :age
   def initialize(info)
-    @color = info[:description].split(' ')[0]
-    @make = info[:description].split(' ')[1]
-    @model = info[:description].split(' ')[2]
-    @year = info[year]
-
+    @color  = info[:description].split(' ')[0]
+    @make   = info[:description].split(' ')[1]
+    @model  = info[:description].split(' ')[2]
+    @year   = info[:year]
+    @age    = 0
   end
 
+  def age
+    @age = Time.now.year - @year.to_i
+  end
 end
