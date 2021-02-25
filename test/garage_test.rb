1b361199fca5aa2ba68173406ef70def8e7daaec
brueck1988 <brueck1988@gmail.com>
2021-02-25 11:16:02 -0700

    Garage all_cars test created

--- test/garage_test.rb
+++ test/garage_test.rb
@@ -15,7 +15,7 @@ class GarageTest < Minitest::Test
     assert_equal 'Totally Safe Parking', garage.name
   end
 
-  def test_it_can_have_different_attributes
+  def test_it_add_customer
     garage = Garage.new('Totally Safe Parking')
     owner_1 = Owner.new('Regina George', 'Heiress')
     owner_1.buy('1967 Green Ford Mustang')
@@ -23,6 +23,24 @@ class GarageTest < Minitest::Test
     owner_2 = Owner.new('Glen Coco', 'Unknown')
     owner_2.buy('1963 Green Chevrolet Corvette')
     owner_2.buy('2020 Silver Volvo XC90')
-    assert_equal
+    garage.add_customer(owner_1)
+    garage.add_customer(owner_2)
+    assert_equal [owner_1, owner_2], garage.customers
+  end
+
+  def test_all_cars
+    garage = Garage.new('Totally Safe Parking')
+    owner_1 = Owner.new('Regina George', 'Heiress')
+    owner_1.buy('1967 Green Ford Mustang')
+    owner_1.buy('2001 Blue Ford Escape')
+    owner_2 = Owner.new('Glen Coco', 'Unknown')
+    owner_2.buy('1963 Green Chevrolet Corvette')
+    owner_2.buy('2020 Silver Volvo XC90')
+
+    garage.add_customer(owner_1)
+    garage.add_customer(owner_2)
+    require "pry";binding.pry
+    expected = [garage.customers[0].cars, garage.customers[1].cars].flatten
+    assert_equal expected, garage.all_cars
   end
 end
