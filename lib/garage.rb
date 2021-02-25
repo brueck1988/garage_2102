9f7d7fa0b9cd42ce4976c1cc36df99f690be4a24
brueck1988 <brueck1988@gmail.com>
2021-02-25 11:31:50 -0700

    Garage class updated to include all_cars

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,12 +1,24 @@
 class Garage
   attr_reader :name,
-              :customers
+              :customers,
+              :all_cars
+
   def initialize(name)
     @name = name
     @customers = []
+    @all_cars = []
   end
 
   def add_customer(owner)
     @customers << owner
+  end
+
+  def all_cars
+    @customers.each do |customer|
+      customer.cars.each do |car|
+        @all_cars << car
+      end
+    end
+    @all_cars
   end
 end
